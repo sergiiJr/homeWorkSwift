f80e1a53c743f8fbecca70e7eb65082ebaf911aa
Sergii <sergiibala@Sergiis-MacBook-Pro.local>
2017-12-14 02:27:36 -0500

    New File

---
+++ LBasics.playground/Contents.swift
@@ -0,0 +1,15 @@
+
+let redColor = "Red"
+let greeColor = "green"
+let blueColor = "blue"
+var color  : String
+
+//homeWork: The Basics
+
+let firstName = "Bob"
+let lastName = "McNorry"
+let height = 6
+let weight = 200
+let age = 23
+print("Student's name is \(firstName)\nStudent's last name is \(lastName)\nStudent's height are \(height) feet\nStudent's age is \(age)")
+
